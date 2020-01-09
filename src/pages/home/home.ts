7d1732afbf51dd324445d0263dfdbc830dfbcb60
Rui Andrade <ruiandrade@outlook.com>
2020-01-09 20:43:37 +0000

    Initial commit. Some work on frontend

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
@@ -6,9 +6,23 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  listStops : any[] = [];
+  @ViewChild("stopInput") stopInput;
 
   constructor(public navCtrl: NavController) {
-
+    this.listStops.push({title: "MOD2", buses: [{destination: "HSJ", time: "1"}, {destination: "HSJ", time: "1"}]})
+    this.listStops.push({title: "RCT1", buses: [{destination: "HSJ", time: "1"}, {destination: "HSJ", time: "1"}]})
+    this.listStops.push({title: "RCT2", buses: [{destination: "HSJ", time: "1"}, {destination: "HSJ", time: "1"}]})
+    this.listStops.push({title: "MOD1", buses: [{destination: "HSJ", time: "1"}, {destination: "HSJ", time: "1"}]})
+    this.listStops.push({title: "SMM1", buses: [{destination: "HSJ", time: "1"}, {destination: "HSJ", time: "1"}]})
+    this.listStops.push({title: "SMM1", buses: [{destination: "HSJ", time: "1"}, {destination: "HSJ", time: "1"}]})
   }
 
+  submitSearch(e) {
+    this.stopInput.value = "";
+  }
+
+  clearSearch(e) {
+    this.stopInput.value = "";
+  }
 }
