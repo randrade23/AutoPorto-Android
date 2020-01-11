5fad38c6aeb0a48255530aefb6ec99303aae6510
Rui Andrade <ruiandrade@outlook.com>
2020-01-11 22:20:48 +0000

    Add search input and start working on app logic; bus stops added to a list which is rendered through template, refresh the information in it every 5 seconds. (Parser things need to be moved into stop provider...)

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,28 +1,65 @@
 import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { StopProvider } from '../../providers/stop/stop';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  listStops : any[] = [];
+  listStops: any[] = [];
+  searchedStops: Array<string> = [];
   @ViewChild("stopInput") stopInput;
 
-  constructor(public navCtrl: NavController) {
-    this.listStops.push({title: "MOD2", buses: [{destination: "HSJ", time: "1"}, {destination: "HSJ", time: "1"}]})
-    this.listStops.push({title: "RCT1", buses: [{destination: "HSJ", time: "1"}, {destination: "HSJ", time: "1"}]})
-    this.listStops.push({title: "RCT2", buses: [{destination: "HSJ", time: "1"}, {destination: "HSJ", time: "1"}]})
-    this.listStops.push({title: "MOD1", buses: [{destination: "HSJ", time: "1"}, {destination: "HSJ", time: "1"}]})
-    this.listStops.push({title: "SMM1", buses: [{destination: "HSJ", time: "1"}, {destination: "HSJ", time: "1"}]})
-    this.listStops.push({title: "SMM1", buses: [{destination: "HSJ", time: "1"}, {destination: "HSJ", time: "1"}]})
+  constructor(public navCtrl: NavController, public stopProvider: StopProvider) {
+    this.listStops = [];
+    this.searchedStops = [];
+    
+    setInterval(() => {
+      this.refreshStops;
+    }, 5000);
   }
 
   submitSearch(e) {
+    var stop = this.stopInput.value.toUpperCase();
+    
+    if (this.searchedStops.indexOf(stop) == -1) {
+      this.searchedStops.push(stop);
+      this.refreshStops();
+    }
+
     this.stopInput.value = "";
   }
 
   clearSearch(e) {
     this.stopInput.value = "";
+  }
+
+  refreshStops() {
+    this.searchedStops.forEach((stop : string, idx, arr) => {
+      this.stopProvider.getStop(stop)
+        .then((response : any) => {
+          var data = response.data;
+
+          var parser = new DOMParser();
+          var htmlDoc = parser.parseFromString(data, 'text/html');
+          
+          var result = this.listStops.find((element, index, obj) => element.title == stop);
+          var resultExists = !(result == undefined);
+
+          if (result == undefined) result = { title: stop, buses: [] };
+          else result.buses = [];
+          
+          var tableRows : HTMLTableRowElement[] = Array.from(htmlDoc.querySelectorAll('tr.even'));
+          tableRows.forEach((row : HTMLTableRowElement, i, tr) => {
+            var line = row.querySelectorAll("td > ul > li > a")[0].innerHTML;
+            var time = row.querySelectorAll("td > i")[0].innerHTML;
+            result.buses.push({time, line});
+          });
+
+          console.log(result);
+          if (!resultExists) this.listStops.push(result);
+        })
+    });
   }
 }
